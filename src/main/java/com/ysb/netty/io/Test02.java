45f8340c618228c81dc2b48120e6ec0157240521
huangbangbang <huangbangbang@163.com>
2021-11-18 11:16:05 +0800

    jot可以查看Java对象内存布局

--- src/main/java/com/ysb/netty/io/Test02.java
+++ src/main/java/com/ysb/netty/io/Test02.java
@@ -1,10 +1,5 @@
 package com.ysb.netty.io;
 
-import java.sql.ResultSet;
-import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.Date;
-
 public class Test02 {
     public static void main(String[] args) {
         /*System.out.println(LocalDateTime.now());
